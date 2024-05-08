6d387c252eedd8905deda73c8bfdb6d357842740
nuclei272 <133608602+nuclei272@users.noreply.github.com>
2024-05-08 20:40:29 +0100

    Add files via upload

---
+++ app/@comments/comments/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex w-full h-12 items-center justify-center bg-yellow-800">
+      <h1 className="text-xl text-white">Loading Comments on "/comments"...</h1>
+    </div>
+  )
+}
